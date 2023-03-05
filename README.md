c75494c1a8f421331c482d4a56231475149431cd
Ophilia Fradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2023-03-05 23:05:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
+
+This is just a test code to learn about local enviromennt and hosting the game online 
---
+++ sketch.js
@@ -0,0 +1,14 @@
+var box;
+
+function setup(){
+  createCanvas(600,800);
+
+  box = createSprite(200,200);
+  box.shapeColor = "black";
+}
+
+function draw(){
+ background("gray");
+
+ drawSprites()
+}
